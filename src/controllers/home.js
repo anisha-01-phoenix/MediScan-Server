c2dd6662e7f902dee028c0460fb206d16e4e9c6c
anisha-01-phoenix <anisha.dat01@gmail.com>
2022-05-14 21:55:52 +0530

    init3

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,7 +1,7 @@
 //returns views/index.html
 const path = require("path");
 const home = (req, res) => {
-  return res.sendFile(path.join(`${__dirname}/../views/index.html`));
+  return res.sendFile(path.join(`${__dirname}../../views/index.html`));
 };
 module.exports = {
   getHome: home
