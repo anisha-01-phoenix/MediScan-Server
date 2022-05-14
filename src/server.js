1661d2ba369d079a2c75eb65f9caad9f006d40f6
anisha-01-phoenix <anisha.dat01@gmail.com>
2022-05-14 22:10:20 +0530

    test

--- src/server.js
+++ src/server.js
@@ -1,16 +1,20 @@
 //initializes routes, configure CORS, runs Express app.
 const cors = require("cors");
 const express = require("express");
-
+const dotenv = require('dotenv')
 const app = express();
 const initRoutes = require("./routes");
 var corsOptions = {
   origin: "http://localhost:8081"
 };
+
+dotenv.config({
+  path: './config/config.env'
+});
 app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 initRoutes(app);
-let port = 8080;
+const port=process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Running at localhost:${port}`);
 });
